54be51794f2d38ecce35789c9ccb494e21e239b1
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-02-06 14:07:10 -0700

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,18 +1,1 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'lAyf1rRlzpuBQuGL1iz8asTsq',
-  consumer_secret: 'Ud4wSSQDWK2xmBHO0cVdKiKpCDJcNJchmsEOhXAu38tQftpm55',
-  access_token_key: '79089479-7tZP0i43qKL8Ju0QOYxZwL6suDYtxGberHNI8GFOv',
-  access_token_secret: 'gxLR1HZ8fJsSogG8L5FVkdMo4SDbXOzDiNtzN0yfUuZNy',
-}
-
-// module.exports = {twitterKeys, spotifyKeys};
-
-
- 
-exports.spotifyKeys =  {
-  id: '0a01992a53f44ecab355dc63622092b4',
-  secret: 'ef97c515d01147d1904a01d748e0981d',
-};
 
